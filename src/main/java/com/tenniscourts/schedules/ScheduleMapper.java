e8cc7b41a77b701f982cec0c352a99da7da0ec68
RJUNIO34 <rjunio34@ford.com>
2021-09-10 00:14:21 -0300

    feat(schedule): implemented service methods

--- src/main/java/com/tenniscourts/schedules/ScheduleMapper.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleMapper.java
@@ -1,14 +1,15 @@
 package com.tenniscourts.schedules;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ScheduleMapper {
-
     Schedule map(ScheduleDTO source);
 
+    @InheritInverseConfiguration
     ScheduleDTO map(Schedule source);
 
     List<ScheduleDTO> map(List<Schedule> source);
--- src/main/java/com/tenniscourts/schedules/ScheduleService.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleService.java
@@ -1,32 +1,44 @@
 package com.tenniscourts.schedules;
 
+import com.tenniscourts.exceptions.EntityNotFoundException;
+import com.tenniscourts.reservations.ReservationRepository;
+import com.tenniscourts.tenniscourts.TennisCourtMapper;
+import com.tenniscourts.tenniscourts.TennisCourtRepository;
+import com.tenniscourts.tenniscourts.TennisCourtService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class ScheduleService {
-
+    private final TennisCourtService tennisCourtService;
+    private final TennisCourtMapper tennisCourtMapper;
     private final ScheduleRepository scheduleRepository;
-
     private final ScheduleMapper scheduleMapper;
 
+    @Transactional
     public ScheduleDTO addSchedule(Long tennisCourtId, CreateScheduleRequestDTO createScheduleRequestDTO) {
-        //TODO: implement addSchedule
-        return null;
+        return scheduleMapper.map(scheduleRepository.saveAndFlush(Schedule.builder()
+                        .startDateTime(createScheduleRequestDTO.getStartDateTime())
+                        .tennisCourt(tennisCourtMapper.map(tennisCourtService.findTennisCourtById(tennisCourtId)))
+                .build()));
+
     }
 
     public List<ScheduleDTO> findSchedulesByDates(LocalDateTime startDate, LocalDateTime endDate) {
-        //TODO: implement
-        return null;
+        return scheduleRepository.findByStartDateTimeBeforeAndEndDateTimeAfter(startDate, endDate).stream()
+                .map(scheduleMapper::map)
+                .collect(Collectors.toList());
     }
 
     public ScheduleDTO findSchedule(Long scheduleId) {
-        //TODO: implement
-        return null;
+        return scheduleMapper.map(scheduleRepository.findById(scheduleId)
+                .orElseThrow(() -> new EntityNotFoundException("Schedule with id " + scheduleId + " not found.")));
     }
 
     public List<ScheduleDTO> findSchedulesByTennisCourtId(Long tennisCourtId) {
